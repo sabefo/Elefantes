a0e6d203d5928f5f37dbf266f54e709b03fb3342
sabefo <santiagofortes@gmail.com>
2015-06-03 11:26:15 -0500

    Metodo de la cancion de los elefantes

---
+++ Elefantes.rb
@@ -0,0 +1,10 @@
+#METODO QUE RECIBE UN NUMERO Y REGRESA LA CANCION DE LOS ELEFANTES
+def cancion(i)
+	for n in 1..i
+		puts "#{n} elefante se columpiaba sobre la tela de una araña, como veía que resistía fuero a llamar a otro elefante."
+#		puts
+	end
+	return
+end
+
+print cancion(5)
